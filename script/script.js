141d5fb17592e512b659ff2b19b289d11678b53b
lochanathiwanka <lochanathiwanka62@gmail.com>
2021-03-05 12:45:35 +0530

    Updated

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function showManageItemForm() {
+    document.getElementById("manageItemForm").style.display = "block";
+    document.getElementById("placeOrderForm").style.display = "none";
+}
+
+function showPlaceOrderForm() {
+    document.getElementById("placeOrderForm").style.display = "block";
+    document.getElementById("manageItemForm").style.display = "none";
+}
